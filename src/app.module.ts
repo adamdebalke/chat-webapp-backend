ec476349cd9c1e408a0537d1c605686a23e80e03
adamdebalke <youngadam65@gmail.com>
2022-09-05 22:58:14 +0300

    Set up Auth Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath: '.env.development' }), AuthModule],
